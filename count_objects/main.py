53ed95ca153bdb2c0fa0f9768ae1f7d1524662cc
Puslore <mini.big10@mail.ru>
2025-03-01 20:15:49 +0800

    count_objects_01_03_2025

--- count_objects/main.py
+++ count_objects/main.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,11 +7,13 @@ external = np.diag([1, 1, 1, 1]).reshape(4, 2, 2)
 
 internal = np.logical_not(external)
 
-cross = np.array([[[1, 0], [0, 1]], [[0, 1], [1, 0]]])
+cross = np.array([[[1, 0], [0, 1]],
+                           [[0, 1], [1, 0]]])
 
 
 def match(a, masks):
     for mask in masks:
+        a = (a != 0).astype(int)
         if np.all(a == mask):
             return True
     return False
@@ -28,3 +31,27 @@ def count_objects(image):
             elif match(sub, cross):
                 E += 2
     return E / 4
+
+
+# --------------------------------------------
+# тут решение для первого файла
+# image = np.load('example1.npy')
+
+# image.shape
+
+# print(count_objects(image))
+
+# plt.imshow(image)
+# plt.show()
+# --------------------------------------------
+
+
+# тут решение для второго файла
+image = np.load('example2.npy')
+
+cnt = 0
+for i in range(image.shape[2]):
+    cnt += count_objects(image[:, :, i])
+    # print(count_objects(image[:, :, i]))
+
+cnt
