f94ac130a56f206fa6bf9da36a46c46a818a4425
Puslore <mini.big10@mail.ru>
2025-05-10 22:07:23 +0800

    remote_stars task done

--- remote_stars/main.py
+++ remote_stars/main.py
@@ -1,17 +1,33 @@
 import matplotlib.pyplot as plt
-import socket 
+import socket
 import numpy as np
 
+
 host = "84.237.21.36"
-port = 5151
+port = 5152
+
+
+def distance(img):
+    res = []
+    for y in range(1, img.shape[0] - 1):
+        for x in range(1, img.shape[1] - 1):
+            neighbors = np.array([img[y + 1, x], img[y, x + 1],
+                                                 img[y - 1, x], img[y, x - 1]])
+            if img[y,x] > np.max(neighbors):
+                res.append(y)
+                res.append(x)
+    return res
+
 
 def recvall(sock, n):
     data = bytearray()
     while len(data) < n:
-        packet = sock.recv(n-len(data))
+        packet = sock.recv(n - len(data))
         if not packet:
             return None
+
         data.extend(packet)
+
     return data
 
 
@@ -24,24 +40,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     plt.figure()
 
     while beat != b"yep":
-
         sock.send(b"get")
-        bts = recvall(sock, 80004)
+        bts = recvall(sock, 40002)
 
-        im1 = np.frombuffer(bts[2:40002], dtype = "uint8").reshape(bts[0], bts[1])
-        im2 = np.frombuffer(bts[40004:], dtype = "uint8").reshape(bts[40002], bts[40003])
-
-        pos1 = np.unravel_index(np.argmax(im1), im1.shape)
-        pos2 = np.unravel_index(np.argmax(im2), im2.shape)
-        result = np.abs(np.array(pos1) - np.array(pos2))
-        sock.send(f"{result[0]} {result[1]}".encode())
+        img = np.frombuffer(bts[2:], dtype = "uint8").reshape(bts[0], bts[1])
+        coords = distance(img)
+        result = ((coords[0] - coords[2]) ** 2 + \
+                      (coords[1] - coords[3]) ** 2) ** 0.5
+        sock.send(f"{round(result, 1)}".encode())
         print(sock.recv(10))
+        
         plt.clf()
-        plt.subplot(121)
-        plt.imshow(im1)
-        plt.subplot(122)
-        plt.imshow(im2)
+        plt.imshow(img)
         plt.pause(1)
-
         sock.send(b"beat")
         beat = sock.recv(10)
